1cafe86e94af9d4ed2533d55601f5b6d4b400218
tamirgoldd <tamir.gold@darrow.ai>
2024-12-28 19:14:33 +0200

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : <Component {...pageProps} />}
+    </div>
+  )
+}
+
+export default MyApp
